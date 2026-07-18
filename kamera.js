7f56ba1d95c70e181d89b6f506db954500779519
Rohmatul Kamilah <62320724+rkmila20@users.noreply.github.com>
2026-07-18 21:29:56 +0700

    kamera.js

--- kamera.js
+++ kamera.js
@@ -7,6 +7,12 @@
 const video = document.getElementById("video");
 const btnBuka = document.getElementById("btnBuka");
 
+const canvas = document.getElementById("canvas");
+const preview = document.getElementById("preview");
+
+const btnFoto = document.getElementById("btnFoto");
+const btnUlang = document.getElementById("btnUlang");
+
 let stream = null;
 
 /************************************************
@@ -47,3 +53,50 @@ async function bukaKamera() {
     }
 
 }
+/************************************************
+ * AMBIL FOTO
+ ************************************************/
+
+btnFoto.addEventListener("click", ambilFoto);
+
+function ambilFoto(){
+
+    if(!stream){
+        alert("Silakan buka kamera terlebih dahulu.");
+        return;
+    }
+
+    if(video.videoWidth === 0){
+        alert("Kamera masih memuat. Tunggu 1 detik lalu coba lagi.");
+        return;
+    }
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    const ctx = canvas.getContext("2d");
+
+    ctx.drawImage(video,0,0);
+
+    const hasil = canvas.toDataURL("image/jpeg",0.9);
+
+    preview.src = hasil;
+
+    preview.style.display = "block";
+    video.style.display = "none";
+
+    btnUlang.style.display = "block";
+
+}
+/************************************************
+ * ULANGI FOTO
+ ************************************************/
+
+btnUlang.addEventListener("click", ()=>{
+
+    preview.style.display = "none";
+    video.style.display = "block";
+
+    btnUlang.style.display = "none";
+
+});
