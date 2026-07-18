e2680ca1aad80e7c1d2583723b94daed7a822d45
Rohmatul Kamilah <62320724+rkmila20@users.noreply.github.com>
2026-07-18 21:13:39 +0700

    kamera.js

--- kamera.js
+++ kamera.js
@@ -1,1 +1,49 @@
+/************************************************
+ * kamera.js
+ * TAHAP 3.1
+ * BUKA KAMERA LIVE
+ ************************************************/
 
+const video = document.getElementById("video");
+const btnBuka = document.getElementById("btnBuka");
+
+let stream = null;
+
+/************************************************
+ * BUKA KAMERA
+ ************************************************/
+
+btnBuka.addEventListener("click", bukaKamera);
+
+async function bukaKamera() {
+
+    // Jika kamera sudah aktif, jangan buka lagi
+    if (stream) return;
+
+    try {
+
+        stream = await navigator.mediaDevices.getUserMedia({
+
+            video: {
+                facingMode: "user",
+                width: { ideal: 1280 },
+                height: { ideal: 720 }
+            },
+
+            audio: false
+
+        });
+
+        video.srcObject = stream;
+
+        console.log("Kamera berhasil dibuka.");
+
+    } catch (err) {
+
+        console.error(err);
+
+        alert("Tidak dapat membuka kamera.");
+
+    }
+
+}
